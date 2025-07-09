52ef3febb7049806e55ab6c56c3673517be2f3f1
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-07-09 15:07:18 +0200

    trying to make a basic gui

--- metaview/app.py
+++ metaview/app.py
@@ -1,20 +1,51 @@
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QHBoxLayout,
+    QWidget
+)
 import sys
 
-class MainWindow(QMainWindow):
+class MetaView(QMainWindow):
     def __init__(self):
         super().__init__()
-        
         self.setWindowTitle("MetaView")
-        button = QPushButton("Click me!")
+        self.setMinimumSize(QSize(400, 550))
         
-        self.setCentralWidget(button)
+        layout = QHBoxLayout()
+        layoutV1 = QVBoxLayout()
+        layoutV2 = QVBoxLayout()
+        
+        layout.addLayout(layoutV1)
+        layout.addLayout(layoutV2)
+        
+        self.button1 = QPushButton("Click me!")
+        self.button1.clicked.connect(self.clicked_button1)
+        
+        self.button2 = QPushButton("Click me!")
+        self.button2.clicked.connect(self.clicked_button2)
+        
+        layoutV1.addWidget(self.button1, alignment=Qt.AlignTop)
+        layoutV2.addWidget(self.button2, alignment=Qt.AlignTop)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        self.setCentralWidget(widget)
+
+    def clicked_button1(self):
+        print("Clicked button 1")
+    
+    def clicked_button2(self):
+        print("Clicked button 2")
 
 def main():
     app = QApplication(sys.argv)
 
-    window = MainWindow()
+    window = MetaView()
     window.show()
 
     app.exec()
